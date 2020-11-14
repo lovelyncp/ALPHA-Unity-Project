468e80bebd92d61892694dcc81ba72fdbb87f287
Lyn <67870065+lynjpg@users.noreply.github.com>
2020-11-14 14:41:31 +0000

    Scaling and Player Control

--- Run!/Assets/Scripts/PlayerController.cs
+++ Run!/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 12.0f;
+    public float speed = 5.0f;
     public float turnSpeed = 20.0f;
-    public float xRange = 5.0f;
+    public float xRange = 20.0f;
     private float horizontalInput;
     private float forwardInput;
 
@@ -21,12 +21,14 @@ public class PlayerController : MonoBehaviour
     {
         // create user input controls
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        //forwardInput = Input.GetAxis("Vertical");
 
         // move the player forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
         // turns the player
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * turnSpeed);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // keep player in x bounds
         if (transform.position.x < -xRange)
