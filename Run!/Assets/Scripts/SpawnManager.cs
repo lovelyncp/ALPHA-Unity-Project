629c4a0c2024efc7fd6728c176ab5cdc04b4f5f7
Lyn <67870065+lynjpg@users.noreply.github.com>
2020-11-16 04:11:44 +0000

    Enemy Move FOrward, Destroyed and Spawn Manager
    Added farmer eenmy at top of scene, farmer gameObject moves toward player and is destroyed at bottom bound outside main camera view, currently working on spawn manager

---
+++ Run!/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] farmerPrefabs;
+    private float spawnPosX = 1.0f;
+    private float spawnRangeZ1 = 3.5f;
+    private float spawnRangeZ2 = 8.5f;
+    private float startDelay = 2f;
+    private float spawnInterval = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomFarmers", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnRandomFarmers();
+    }
+
+    void SpawnRandomFarmers()
+    {
+        int farmerIndex = Random.Range(0, farmerPrefabs.Length);
+        Vector3 spawnPos = new Vector3(spawnPosX, -8, Random.Range(-spawnRangeZ1, -spawnRangeZ2));
+        //Instantiate(farmerPrefabs[farmerIndex], spawnPos, farmerPrefabs[farmerIndex].transform.rotation);
+    }
+}
